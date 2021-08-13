40b150975285228414f0148637dee96fe9ff9183
but-sergey <but-sergey@bk.ru>
2021-08-13 16:22:31 +0300

    Заполнители

--- hello.c
+++ hello.c
@@ -2,11 +2,17 @@
 
 int main()
 {
-    printf("Hello world\n");
-    printf("This is a new row");
-    printf("This is a '\t' tab symbol");
-    printf("This is backslash \\");
-    printf("This is a terminant \0 all the other text will be ignored");
+    // printf("Hello world\n");
+    // printf("This is a new row");
+    // printf("This is a '\t' tab symbol");
+    // printf("This is backslash \\");
+    // printf("This is a terminant \0 all the other text will be ignored");
+    
+    int a = 50;
+    printf("%d\n", a);
+    printf("%05d\n", a);
+    printf("%.2f\n", 5.12345);
+    printf("«аполнители - это \"%5d%%\" форматировани€ текста!", a);
 
     return 0;
 }
