bfd123c224c6b269a1043a0a4b67e26cddc9e3a7
but-sergey <but-sergey@bk.ru>
2021-08-13 17:32:38 +0300

    Тип char

--- hello.c
+++ hello.c
@@ -11,11 +11,14 @@ int main()
     // printf("This is backslash \\");
     // printf("This is a terminant \0 all the other text will be ignored");
     
-    int a = 50;
-    printf("%d\n", a);
-    printf("%05d\n", a);
-    printf("%.2f\n", 5.12345);
-    printf("«аполнители - это \"%5d%%\" форматировани€ текста!", a);
+    // int a = 50;
+    // printf("%d\n", a);
+    // printf("%05d\n", a);
+    // printf("%.2f\n", 5.12345);
+    // printf("«аполнители - это \"%5d%%\" форматировани€ текста!", a);
+
+    unsigned char symbol = 75;
+    printf("%c\n", symbol);
 
     return 0;
 }
