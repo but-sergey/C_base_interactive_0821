7fa3e3376b28572d20a6de6fb031b63e283b7c8d
but-sergey <but-sergey@bk.ru>
2021-08-13 17:44:25 +0300

    Сложение

--- hello.c
+++ hello.c
@@ -5,13 +5,12 @@ int main()
 {
     setlocale(LC_ALL, "Rus");
     
-    int a = 10;
-    printf("Переменная a имеет значение: %d\n", a);
-    printf("Переменная a хранится по адресу: %p\n", &a);
-    int input;
-    printf("Введите, пожалуйста, число: ");
-    scanf("%d", &input);
-    printf("Вы ввели число %d, мы удвоили его для Вас, и получилось: %d\n", input, input*2);
+    int variable = 70;
+    printf("Переменная vatiable = %d\n", variable);
+    variable = variable + 50;
+    printf("Переменная vatiable = %d\n", variable);
+    variable += 50;
+    printf("Переменная vatiable = %d\n", variable);
 
     return 0;
 }
