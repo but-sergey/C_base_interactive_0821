832c4f35d6efc0ba6a63ca74ed18db58802bb3e0
but-sergey <but-sergey@bk.ru>
2021-08-13 20:42:19 +0300

    ДЗ-1: Задание 4

--- hw1.c
+++ hw1.c
@@ -11,12 +11,30 @@ int main()
 {
     setlocale(LC_ALL, "Rus");
     
-    printf("Hello world!!!\n");
+    // printf("Hello world!!!\n");
 
-    int num;
-    printf("Введите целое число: ");
-    scanf("%d", &num);
-    printf("Вы ввели: %d, квадрат этого числа: %d\n", num, num * num);
+    // int num;
+    // printf("Введите целое число: ");
+    // scanf("%d", &num);
+    // printf("Вы ввели: %d, квадрат этого числа: %d\n", num, num * num);
+
+    float R1 = 1.0f;
+    float R2 = 3.0f;
+    float Ui = 4.0f;
+    float Uo = 1.0f;
+
+    printf("Начальные данные: R1 = %.1f, R2 = %.1f, Ui = %.1f, Uo = %.1f\n", R1, R2, Ui, Uo);
+    printf("Расчеты:\n");
+
+    float Uo_ = Ui * (R1 / (R1 + R2));
+    float Ui_ = Uo * (R1 + R2) / R1;
+    float R1_ = R2 * Uo / (Ui - Uo);
+    float R2_ = R1 * (Ui - Uo) / Uo;
+
+    printf("Uo = %.1f\n", Uo_);
+    printf("Ui = %.1f\n", Ui_);
+    printf("R1 = %.1f\n", R1_);
+    printf("R2 = %.1f\n", R2_);
 
     return 0;
 }
