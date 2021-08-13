cc9e3d6c05d0096a0ba96cf61ce66e7a2573b000
but-sergey <but-sergey@bk.ru>
2021-08-13 16:15:21 +0300

    Форматированный вывод

--- hello.c
+++ hello.c
@@ -3,6 +3,10 @@
 int main()
 {
     printf("Hello world\n");
-    
+    printf("This is a new row");
+    printf("This is a '\t' tab symbol");
+    printf("This is backslash \\");
+    printf("This is a terminant \0 all the other text will be ignored");
+
     return 0;
 }
