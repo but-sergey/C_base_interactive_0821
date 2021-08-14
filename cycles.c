cb38211ad2235072ee4f58d62624da916bfda41c
but-sergey <but-sergey@bk.ru>
2021-08-14 15:31:10 +0300

    Цшкл с постусловием

--- cycles.c
+++ cycles.c
@@ -7,15 +7,20 @@ int main(int argc, const char** argv){
     // {
     //     printf("%d ", ++a);
     // }
-    int even = 0;
-    while (a < b)
-    {
-        if (a % 2 == 0)
-            even++;
-        a++;
-    }
-    printf("There are %d even numbers\n", even);
-        
+    // int even = 0;
+    // while (a < b)
+    // {
+    //     if (a % 2 == 0)
+    //         even++;
+    //     a++;
+    // }
+    // printf("There are %d even numbers\n", even);
+
+    do{
+        printf("%s", "Enter a divider for 100: ");
+        scanf("%d", &a);
+    } while(a == 0);
+    printf("100 / %d = %d\n", a, 100/a);
 
     return 0;
 }
