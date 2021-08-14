2f2a016906da68db8fc9235e1db9d184fe22d33f
but-sergey <but-sergey@bk.ru>
2021-08-14 15:25:13 +0300

    Простой цикл while

---
+++ cycles.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(int argc, const char** argv){
+    int a = 10;
+    int b = 20;
+    while (a < b)
+    {
+        printf("%d ", a);
+        a++;
+    }
+    
+
+    return 0;
+}
