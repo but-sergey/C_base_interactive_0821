dbab49a9eafadaba579ea80514849245620533fb
but-sergey <but-sergey@bk.ru>
2021-08-14 15:47:47 +0300

    Возведение в степень посредством цикла for

--- MoreCycles.c
+++ MoreCycles.c
@@ -4,27 +4,37 @@
 int main(int argc, const char** argv){
     setlocale(LC_ALL, "Rus");
 
-    int number;
-    printf("¬ведите, пожалуйста, число: ");
-    scanf("%d", &number);
-    int d = 0;
-    int i = 1;
-    while (i <= number)
+    // int number;
+    // printf("¬ведите, пожалуйста, число: ");
+    // scanf("%d", &number);
+    // int d = 0;
+    // int i = 1;
+    // while (i <= number)
+    // {
+    //     if (number % i++ == 0)
+    //     {
+    //         d++;
+    //     }
+    //     else
+    //     {
+    //         continue;
+    //     }
+    //     if (d == 3)
+    //     {
+    //         break;
+    //     }
+    // }
+    // printf("¬веденное число %d %s€вл€етс€ простым", number, (d == 2) ? "" : "не ");
+
+    int base = 2;
+    int signigicative = 10;
+    int result = 1;
+
+    for (int i = 0; i < signigicative; i++)
     {
-        if (number % i++ == 0)
-        {
-            d++;
-        }
-        else
-        {
-            continue;
-        }
-        if (d == 3)
-        {
-            break;
-        }
+        result *= base;
     }
-    printf("¬веденное число %d %s€вл€етс€ простым", number, (d == 2) ? "" : "не ");
+    printf("power: %d\n", result);
 
     return 0;
 }
