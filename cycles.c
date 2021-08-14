457104cbce32dcc596cbb9acae60309267366d8e
but-sergey <but-sergey@bk.ru>
2021-08-14 15:27:58 +0300

    Подсчет четных чисел

--- cycles.c
+++ cycles.c
@@ -3,11 +3,19 @@
 int main(int argc, const char** argv){
     int a = 10;
     int b = 20;
+    // while (a < b)
+    // {
+    //     printf("%d ", ++a);
+    // }
+    int even = 0;
     while (a < b)
     {
-        printf("%d ", ++a);
+        if (a % 2 == 0)
+            even++;
+        a++;
     }
-    
+    printf("There are %d even numbers\n", even);
+        
 
     return 0;
 }
