b12cfe70bbd9bdce921511cecf7956024cc0d035
but-sergey <but-sergey@bk.ru>
2021-08-14 15:39:04 +0300

    Определение простого числа

---
+++ MoreCycles.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <locale.h>
+
+int main(int argc, const char** argv){
+    setlocale(LC_ALL, "Rus");
+
+    int number;
+    printf("¬ведите, пожалуйста, число: ");
+    scanf("%d", &number);
+    int d = 0;
+    int i = 1;
+    while (i <= number)
+    {
+        if (number % i++ == 0)
+        {
+            d++;
+        }
+    }
+    printf("¬веденное число %d %s€вл€етс€ простым", number, (d == 2) ? "" : "не ");
+
+    return 0;
+}
