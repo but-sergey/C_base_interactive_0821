385b354a93787af3c56a31ec810a7d49f1d3058d
but-sergey <but-sergey@bk.ru>
2021-08-14 15:20:14 +0300

    Операции сдвига

--- algebra.c
+++ algebra.c
@@ -3,7 +3,7 @@
 int main(int argc, const char** argv){
     char a = 11;
     char b = 15;
-    printf("a = %d, b = %d\n", a, b);
+    /*printf("a = %d, b = %d\n", a, b);
     a = a ^ b;
     b = b ^ a;
     a = a ^ b;
@@ -21,6 +21,13 @@ int main(int argc, const char** argv){
     // b = 11 (00001011)
     // a ^ b = 00001111
     printf("a = %d, b = %d\n", a, b);
-    
+    */
+
+    // x << n = x * 2 ** n
+    // b = 00001111 << 1
+    // b = 00011110 (30)
+    // x >> n = x / 2 ** n
+    // b = 00011110 >> 3
+    // b = 00000011 (3)
     return 0;
 }
